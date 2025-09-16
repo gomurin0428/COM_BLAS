5bc732f9c10621227a107acb46614953434a5a64
小村 豪 <gomurin0428@gmail.com>
2025-09-16 14:25:06 +0900

    ユニットテストプロジェクト追加

---
+++ COM_BLAS_UnitTest_Managed/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace COM_BLAS_UnitTest_Managed
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
